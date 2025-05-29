f2f9f60f10b9e9486864b776d0f9d032d46e439b
enzo <enzogrledo@gmail.com>
2025-05-29 14:51:26 -0300

    teste

---
+++ src/main/java/com/example/app/Personagens.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+public class Personagens 
+{
+    String name;
+    int health;
+    int linha;
+    int coluna;
+
+    public Personagens(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+
+}
---
+++ src/main/java/com/example/app/Tabuleiro.java
@@ -0,0 +1,10 @@
+package com.example.app;
+
+public class Tabuleiro {
+    int lihas = 10;
+    int colunas = 10;
+
+    public void imprimeTabuleiro() {
+        
+    }
+}
