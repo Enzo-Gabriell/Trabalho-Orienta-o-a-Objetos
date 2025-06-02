ceed7c1323a49ac6d98afe84a3ad959915e9fcae
Arthur <arthurborba40@gmail.com>
2025-06-02 13:40:19 -0300

    edit menu

--- src/main/java/com/example/app/menu.java
+++ src/main/java/com/example/app/menu.java
@@ -2,18 +2,24 @@ package com.example.app;
 import javax.swing.JOptionPane;
 
 public class menu {
-    public static void main(String[] args){
+    private static void pvp(){
+        //Personagens personagem1, personagem2;
+        
+
+    }
+        public static void main(String[] args){
+        //printa menu principal
         Object[] options = { "Jogar PvP", "Jogar PvE", "Sair do jogo" };
         int opcao = JOptionPane.showOptionDialog(null, "BEM VINDO!", "Duelo de Personagens", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,null, options, options[2]);
-    
+        
+        //inicia a partida 
         switch (opcao) {
             case 0:
-                JOptionPane.showMessageDialog(null, "Tela PvP");
+                pvp();
                 break;
             case 1:
                 JOptionPane.showMessageDialog(null, "Tela PvE");
                 break;
-
             default:
                 break;
         }
