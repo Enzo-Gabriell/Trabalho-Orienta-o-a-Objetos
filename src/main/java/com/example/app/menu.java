a0a41e888d110e4189b6273f96d6201b350dcc91
Arthur <arthurborba40@gmail.com>
2025-06-09 11:39:08 -0300

    altera menu + adiciona desenhaTabuleiro

--- src/main/java/com/example/app/menu.java
+++ src/main/java/com/example/app/menu.java
@@ -1,27 +1,41 @@
 package com.example.app;
 import javax.swing.JOptionPane;
+import javax.swing.JFrame;
 
 public class menu {
     private static void pvp(){
-        //Personagens personagem1, personagem2;
+        Jogo partida;
         
 
     }
-        public static void main(String[] args){
+    public static void main(String[] args){
         //printa menu principal
         Object[] options = { "Jogar PvP", "Jogar PvE", "Sair do jogo" };
         int opcao = JOptionPane.showOptionDialog(null, "BEM VINDO!", "Duelo de Personagens", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,null, options, options[2]);
         
         //inicia a partida 
-        switch (opcao) {
-            case 0:
-                pvp();
-                break;
-            case 1:
-                JOptionPane.showMessageDialog(null, "Tela PvE");
-                break;
-            default:
-                break;
-        }
+        // switch (opcao) {
+        //     case 0:
+        //         pvp();
+        //         break;
+        //     case 1:
+        //         JOptionPane.showMessageDialog(null, "Tela PvE");
+        //         break;
+        //     default:
+        //         break;
+        // }
+        
+        JFrame frame = new JFrame("Tabuleiro de Xadrez");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(565, 588);
+        frame.setLocationRelativeTo(null);
+        
+        // O painel do tabuleiro será adicionado aqui
+        DesenhaTabuleiro tabuleiro = new DesenhaTabuleiro();
+        frame.add(tabuleiro);
+        
+        frame.setVisible(true);
+	    
+
     }
 }
